ccc7a7b98e82c91a13d57be515306f3861c93a14
sankhet <sankhe.t@northeastern.edu>
2020-01-26 18:45:08 -0800

    unit test modification and config yml fix

--- src/test/java/com/cloud/usermanagement/services/UserServiceTest.java
+++ src/test/java/com/cloud/usermanagement/services/UserServiceTest.java
@@ -8,32 +8,49 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cloud.usermanagement.Exceptions.ValidationException;
 import com.cloud.usermanagement.models.User;
 import com.cloud.usermanagement.repositories.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
-	
+
 	@Autowired
 	private UserService userService;
 
-	@Autowired
-	private UserRepository userRepository;
-	
+	//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@Test
+//	public void TestGetUser() throws Exception
+//	{
+//		User user= new User();
+//		user.setFirstName("Tej");
+//		user.setLastName("Sankhe");
+//		user.setEmailAddress("tejtest@gmail.com");
+//		user.setPassword("Tejtest@123");
+//		userRepository.save(user);
+//		User userRetrived= userService.getUser(user.getEmailAddress());
+//		assertNotNull(userRetrived);
+//		assertEquals("Username match", user.getEmailAddress(), (userRetrived).getEmailAddress());
+//		userRepository.delete((User) userRetrived);
+//	}
+//
 	@Test
-	public void TestGetUser() throws Exception
+	public void createUserInvalidEmailAddress()
 	{
 		User user= new User();
 		user.setFirstName("Tej");
 		user.setLastName("Sankhe");
 		user.setEmailAddress("tejtest@gmail.com");
-		user.setPassword("Tejtest@123");
-		//userRepository.save(user);
-		//User userRetrived= userService.getUser(user.getEmailAddress());
-		//assertNotNull(userRetrived);
-		//assertEquals("Username match", user.getEmailAddress(), (userRetrived).getEmailAddress());
-		//userRepository.delete((User) userRetrived);
-		assertEquals("aa","aa");
+		user.setPassword("Tejtest123");
+		try {
+			User userSaved= userService.save(user);
+		} catch (ValidationException e) {
+			assertEquals(e.getMessage(),"Password should be between 8 and 64 character and it should contain at least one lowercase, uppercase, number, and symbol");
+		}
+
 	}
 }

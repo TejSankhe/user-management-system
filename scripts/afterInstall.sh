3d280b195eadda8209b6160dcd28871d111068d8
sankhet <sankhe.t@northeastern.edu>
2020-03-18 17:42:47 -0400

    added before hook

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -8,9 +8,6 @@ sudo chmod +x demo-0.0.1-SNAPSHOT.jar
 #Killing the application
 kill -9 $(ps -ef|grep demo-0.0.1 | grep -v grep | awk '{print $2}')
 
-# #Removing log files
-#sudo rm -rf logs/*.log
-
 source /etc/profile.d/envvariable.sh
 nohup java -jar demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/log.txt 2> /home/ubuntu/log.txt < /home/ubuntu/log.txt &
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/amazon-cloudwatch-agent-schema.json -s

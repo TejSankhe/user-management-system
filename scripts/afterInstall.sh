31fe28015bcf59b88cf652ace2d36a591b45908a
sankhet <sankhe.t@northeastern.edu>
2020-03-18 13:06:00 -0400

    change to v1

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -9,7 +9,7 @@ sudo chmod +x demo-0.0.1-SNAPSHOT.jar
 kill -9 $(ps -ef|grep demo-0.0.1 | grep -v grep | awk '{print $2}')
 
 # #Removing log files
-# sudo rm -rf logs/*.log
+sudo rm -rf logs/*.log
 
 source /etc/profile.d/envvariable.sh
 nohup java -jar demo-0.0.1-SNAPSHOT.jar > /home/ubuntu/log.txt 2> /home/ubuntu/log.txt < /home/ubuntu/log.txt &
